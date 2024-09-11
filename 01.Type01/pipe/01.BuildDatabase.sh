fbedcf9096043bc20af3637b237c7b7162a9dbc0
kimzz14 <kimzz1004@gmail.com>
2024-09-11 12:21:38 +0900

    update

--- 01.Type01/pipe/01.BuildDatabase.sh
+++ 01.Type01/pipe/01.BuildDatabase.sh
@@ -1,4 +1,15 @@
 dbName=$1
+threadN=$2
+
+if [ -z "${dbName}" ]; then
+    echo "Error: The variable 'Error' is empty."
+    exit 1
+fi
+
+if [ -z "${threadN}" ]; then
+    echo "Error: The variable 'threadN' is empty."
+    exit 1
+fi
 
 cd 01.BuildDatabase
 
